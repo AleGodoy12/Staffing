bd05df5d596bd02a5c7b1981fff78fe76b3003fe
Santiago Baliño <94008211+Luvwen@users.noreply.github.com>
2023-08-08 09:57:00 -0300

    Database connection (#5)
    * Add bcrypt mssql and cors dependencies
    
    * Create successful connection with testing db

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
 require("dotenv").config()
+const db = require("./db/database")
 
-var app = express();
 
+const app = express();
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+db()
 
 module.exports = app;
