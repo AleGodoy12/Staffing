3d8221cad9305dc874a7775a08b54dc9ef760099
Santiago Baliño <94008211+Luvwen@users.noreply.github.com>
2023-08-09 16:20:07 -0300

    Login controllers, routes and validations (#9)
    * Create connection to sql server db
    
    * Add express-session, express-validation and prettier config file
    
    * Update db connection and add an auto-generated user
    
    * Create login routes, controllers and validations
    
    * Add blank spaces validation in both user and password fields

---
+++ server/middlewares/validateLogin.js
@@ -0,0 +1,20 @@
+const { body } = require('express-validator');
+
+const validateLogin = [
+	body('username')
+		.matches('^[0-9a-zA-Z]+$')
+		.withMessage(
+			'Debe contener letras minúsculas o mayúsculas y números sin espacios en blanco.'
+		)
+		.isLength({ min: 3, max: 15 })
+		.withMessage('Debe tener mínimo 3 caracteres y máximo 15'),
+	body('password')
+		.matches('^[0-9a-zA-Z]+$')
+		.withMessage(
+			'Debe contener letras minúsculas o mayúsculas y números sin espacios en blanco.'
+		)
+		.isLength({ min: 8, max: 15 })
+		.withMessage('Debe tener mínimo 8 caracteres y máximo 15'),
+];
+
+module.exports = validateLogin;
