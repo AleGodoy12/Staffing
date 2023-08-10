5353c5ce9050a171b698f6fe6a1074ebd2334d3d
Simon <105747611+SimonSantillan@users.noreply.github.com>
2023-08-10 09:01:49 -0300

    built design and configured dependency (#10)

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,27 @@
+import LogoArbusta from '../assets/icons/arbusta-icon.png'
+
+export default function Login() {
+  return (
+    <>
+      <main className="main-login">
+        <section className="login">
+          <div className="container">
+            <div className="logo">
+              <img src={LogoArbusta} alt="logo" />
+              <p>ARBUSTA</p>
+            </div>
+            <p> Una nueva forma de gestionar nuestros equipos y proyectos</p>
+            <h1>Inicio de sesión</h1>
+            <form action="#">
+              <label htmlFor="user">Usuario</label>
+              <input type="text" name="user" id="user" />
+              <label htmlFor="password">Contraseña</label>
+              <input type="password" name="" id="password" />
+              <input type="submit" value="Iniciar sesión" />
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
