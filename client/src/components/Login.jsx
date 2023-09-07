4710209de96a7300eac310d9fd7d5b1475338788
SimonSantillan <sgeronimosantillan@outlook.com>
2023-09-07 17:20:09 -0300

    refactored login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,16 +11,13 @@ export default function Login() {
 
   const navigate = useNavigate();
 
-
-  const [err, setErr] = useState({
-    back: '',
-    frontUser: '',
-    frontPassword: ''
-  })
   const [data, setData] = useState({
     username: 'admin',
     password:'12345678'
   })
+  const [errorMessageFrontU, setErrorMessageFrontU] = useState('');
+  const [errorMessageFrontP, setErrorMessageFrontP] = useState('');
+  const [errorMessageBack, setErrorMessageBack] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,14 +25,10 @@ export default function Login() {
       ...data,
       [name]: value
     })
-    setErr({
-      back: '',
-      frontUser: '',
-      frontPassword: ''
-    })
+    setErrorMessageFrontU('');
+    setErrorMessageFrontP('');
+    setErrorMessageBack('');
   }
-  
-  
   
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -43,97 +36,68 @@ export default function Login() {
       /* Validaciones del front */
       
       if (data.username.length < 3 && data.username !== '') {
-        setErr({
-					...err,
-					frontUser: 'El usuario no puede tener menos de 3 caracteres',
-				});
-        document.querySelector('input[name=username]').style.borderColor = "red";
+        setErrorMessageFrontU('El usuario no puede tener menos de 3 caracteres');
       } else if (data.username.length > 15 && data.username !== '') {
-        setErr({
-					...err,
-					frontUser: 'El usuario no puede tener más de 15 caracteres',
-				});
-				document.querySelector('input[name=username]').style.borderColor =
-					'red';
-			}
+        setErrorMessageFrontU('El usuario no puede tener más de 15 caracteres');
+      }
       if (data.password.length < 8 && data.password !== '') {
-        setErr({
-					...err,
-					frontPassword: 'La contraseña no puede tener menos de 8 caracteres',
-				});
-				document.querySelector('input[name=password]').style.borderColor =
-					'red';
-			} else if (data.password.length > 15 && data.password !== '') {
-        setErr({
-          ...err,
-          frontPassword:'La contraseña no puede tener más de 15 caracteres'
-        })
-				document.querySelector('input[name=password]').style.borderColor =
-					'red';
-			}
+        setErrorMessageFrontP('La contraseña no puede tener menos de 8 caracteres');
+      } else if (data.password.length > 15 && data.password !== '') {
+        setErrorMessageFrontP('La contraseña no puede tener más de 15 caracteres');
+      }
 
-
-      if (err.frontPassword !== '' || err.frontUser !== '') {
+      if (errorMessageFrontP !== '' || errorMessageFrontU !== '') {
         return;
       }
-      
-      console.log(data.username)
-      console.log(data.password)
+
       const response = await axios.post(url, {
         username: data.username,
-				password: data.password,
+        password: data.password,
       });
-      console.log(response);
       
       navigate('/panel');
-
     } catch (error) {
-      console.log(error)
-      let errorResponse = error.response.data.errorDetail;
-      console.log(errorResponse)
-      setErr({
-        back: errorResponse,
-        ...err,
-      });
-      
-		}
-	};
-
+      const errorResponse = error.response.data.errorDetail;
+      setErrorMessageBack(errorResponse);
+    }
+  };
 
   return (
-		<>
-			<main className="main-login">
-				<section className="login">
-					<div className="container">
-						<div className="logo">
-							<img src={LogoArbusta} alt="logo" />
-							<p>ARBUSTA</p>
-						</div>
-						<p> Una nueva forma de gestionar nuestros equipos y proyectos</p>
-						<h1>Inicio de sesión</h1>
-						<form action="#">
-              <label htmlFor="user">Usuario: <span>{ err.frontUser }</span></label>
-							<input
-								type="text"
-								name="username"
-								value={data.username}
-								onChange={handleChange}
-								id="user"
-							/>
-              <label htmlFor="password">Contraseña: <span>{ err.frontPassword }</span> </label>
-							<input
-								type="password"
-								name="password"
-								id="password"
-								value={data.password}
-								onChange={handleChange}
-							/>
-							<button onClick={handleLogin}>Iniciar sesión</button>
-						</form>
-            <span>{ err.back }</span>
-					</div>
-				</section>
-			</main>
-		</>
-	);
+    <>
+      <main className="main-login">
+        <section className="login">
+          <div className="container">
+            <div className="logo">
+              <img src={LogoArbusta} alt="logo" />
+              <p>ARBUSTA</p>
+            </div>
+            <p> Una nueva forma de gestionar nuestros equipos y proyectos</p>
+            <h1>Inicio de sesión</h1>
+            <form action="#">
+              <label htmlFor="user">Usuario: <span>{ errorMessageFrontU }</span></label>
+              <input
+                style={errorMessageFrontU !== '' ? {borderColor:"red"} : {borderColor:"#afa2c3"}}
+                type="text"
+                name="username"
+                value={data.username}
+                onChange={handleChange}
+                id="user"
+              />
+              <label htmlFor="password">Contraseña: <span>{ errorMessageFrontP }</span> </label>
+              <input
+                style={errorMessageFrontP !== '' ? {borderColor:"red"} : {borderColor:"#afa2c3"}}
+                type="password"
+                name="password"
+                id="password"
+                value={data.password}
+                onChange={handleChange}
+              />
+              <button onClick={handleLogin}>Iniciar sesión</button>
+            </form>
+            <span>{ errorMessageBack }</span>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
