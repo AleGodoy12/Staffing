d316fadf15b07e33213f0fa1616995cbb4dd26ed
SimonSantillan <sgeronimosantillan@outlook.com>
2023-09-11 17:54:46 -0300

    Create assignment project, create a modal assign and fix tabs and code spacing

--- client/src/components/assignament/AssignmentProject.jsx
+++ client/src/components/assignament/AssignmentProject.jsx
@@ -5,6 +5,7 @@ import BackIcon from '../../assets/icons/back-icon.svg';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
+import AssignModal from './AssignModal';
 
 
 /* Llamo al endpoint del proyecto */
@@ -12,14 +13,19 @@ const url = 'http://localhost:3000/';
 /* Llamo al endpoint del proyecto que me trae empleados libres */
 const urlEmployee = 'http://localhost:3000/project/viewFreeEmployes';
 /* Llamo al endpoint del proyecto que me trae empleados asignados al proyecto  */
-const urlEmployeeFromProject = 'http://localhost:3000/project/showEmployees';
+const urlEmployeeFromProject = 'http://localhost:3000/project/showEmployees/';
 
 export default function AssignmentProject() {
-  const [projectName, setProjectName] = useState('');
-	const [projectArea, setProjectArea] = useState('');
-	const [projectStart, setProjectStart] = useState('');
-	const [projectEnd, setProjectEnd] = useState('');
-  const [projectHours, setProjectHours] = useState(0);
+  const [hoursToAssign, setHoursToAssign] = useState(0)
+  const [project, setProject] = useState({
+    name: '',
+    area: '',
+    start: '',
+    end: '',
+    hours: 0,
+    assignedHours: 0,
+    idProject:0
+  })
   /* empleados */
   const [employee, setEmployee] = useState([])
 
@@ -29,27 +35,42 @@ export default function AssignmentProject() {
   const getProjectById = async () => {
     const res = await axios.get(url + id);
     const data = res.data.projects[0];
-    console.log(data);
-    setProjectName(data.name_project);
-		setProjectArea(data.area_project);
-		setProjectStart(data.start_date_project.split('T')[0]);
-		setProjectEnd(data.end_date_project.split('T')[0]);
-		setProjectHours(data.hours_estimation);
+    console.log(data, 'proyecto data');
+    setProject({
+			name: data.name_project,
+			area: data.area_project,
+			start: data.start_date_project.split('T')[0],
+			end: data.end_date_project.split('T')[0],
+      hours: data.hours_estimation,
+      assignedHours: data.assigned_hours,
+      idProject: data.id_project
+    });
+    
   }
 
   const getFreeEmployees = async () => {
     const res = await axios.get(urlEmployee);
     const data = res.data.data;
     setEmployee(data);
-    console.log(data);
+    console.log(data, 'Empleados libres');
   }
 
   
   const viewEmployeeFromSelectedProject = async () => {
-    const res = await axios.get(urlEmployeeFromProject, {
-      params: { id }
-    })
-    console.log(res);
+    const id_project = id;
+    const res = await axios.get(urlEmployeeFromProject+id_project)
+    /* console.log(res, 'Empleados del proyecto seleccionado'); */
+  }
+
+  const assignEmployee = async (proyecto, empleado, horas) => {
+    let project_id = proyecto;
+    let employee_id = empleado;
+    let hours_to_assign = horas;
+
+    const res = await axios.post(`${url}project/${project_id}/${employee_id}/${hours_to_assign}`)
+    getProjectById();
+		getFreeEmployees();
+		viewEmployeeFromSelectedProject();
   }
 
   useEffect(() => {
@@ -58,7 +79,7 @@ export default function AssignmentProject() {
     viewEmployeeFromSelectedProject();
 	}, []);
 
-	return (
+  return (
 		<>
 			<main className="main-home">
 				<Sidebar></Sidebar>
@@ -73,42 +94,76 @@ export default function AssignmentProject() {
 						</div>
 						<section className="data">
 							<div>
-								<div className="texto">
-									<p>
-										Nombre del proyecto: <span>{projectName} </span>
-									</p>
-									<p>
-										Área del proyecto: <span>{projectArea} </span>
-									</p>
-									<p>
-										Tiempo estimado:
-										<span>Inicio: {projectStart}</span>
-										<span>Fin: {projectEnd}</span>
-									</p>
+								<div className="table">
+									<div className="td">
+										<h3>Nombre del proyecto</h3>
+										<p>{project.name}</p>
+									</div>
+									<div className="td">
+										<h3>Área del proyecto</h3>
+										<p>{project.area}</p>
+									</div>
+									<div className="td">
+										<h3>Tiempo estimado</h3>
+										<p>
+											{project.start} <br /> {project.end}
+										</p>
+									</div>
 								</div>
 								<div className="bar">
 									<h2>
-										Horas del proyecto: <span>{projectHours}hs</span>
+										Horas del proyecto: <span>{project.hours}hs</span>
 									</h2>
 									<p>Horas asignadas:</p>
-									<progress max="100" value="70"> </progress>
+									<div>
+										<progress
+											value={
+												isNaN((project.assignedHours * 100) / project.hours)
+													? 0
+													: (project.assignedHours * 100) / project.hours
+											}
+											max="100"
+										></progress>
+										<p>
+											{project.assignedHours}hs/{project.hours}hs
+										</p>
+									</div>
 								</div>
 							</div>
-              <div>
-                <p>Staff Seleccionado</p>
-              </div>
+							<div>
+								<p>Staff Seleccionado</p>
+							</div>
+						</section>
+						<section className="employee">
+							{employee.map((e, index) => (
+								<div
+									className="cardEmployee"
+									key={index}
+								>
+									<h1>
+										{e.name} {e.lastname}
+									</h1>
+									<p>Horas Disponibles: {e.free_hours}</p>
+									<p>Horas Usadas: {e.used_hours} </p>
+									<p>Horas Totales: {e.total_hours}</p>
+                  <AssignModal>
+                    <p>Seleccione la cantidad de horas</p>
+                    <input
+                      onChange={(e)=> setHoursToAssign(e.target.value)}
+                      type="number"
+                      name="assignHours"
+                      id=""
+                      required
+                      
+                    />
+                    {hoursToAssign}
+                    {project.idProject}
+                    <button onClick={() => assignEmployee(project.idProject, e.id_employee, hoursToAssign)}> Asignar </button>
+                  </AssignModal>
+								</div>
+							))}
             </section>
-            <section className="employee">
-              {employee.map((e,index) => (
-                <div className="card" key={index} style={{border: "1px solid black"}}>
-                  <h1>{e.name} {e.lastname }</h1>
-                  <p>Horas Disponibles: {e.free_hours }</p>
-                  <p>Horas Usadas: {e.used_hours} </p>
-                  <p>Horas Totales: {e.total_hours }</p>
-                </div>
-              )) }
-
-            </section>
+            
 					</section>
 				</section>
 			</main>
