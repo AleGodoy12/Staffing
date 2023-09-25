da2b0b9baf2ae903e2384981241adc58997452a4
Simon <105747611+SimonSantillan@users.noreply.github.com>
2023-09-25 17:20:24 -0300

    Add filter funcionality (#31)
    * add filter funcionality
    
    * Add logic to filter functionality

--- client/src/components/assignament/AssignModal.jsx
+++ client/src/components/assignament/AssignModal.jsx
@@ -2,42 +2,40 @@ import { useState } from 'react';
 import '../../assets/css/AssignModal.css'
 import axios from 'axios';
 
-
 const url = 'http://localhost:3000/';
 
 export default function AssignModal({ idProject, idEmployee, load, nameSurname, freeHours, usedHours }) {
-	const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-	const openM = () => {
-		setOpen(true);
-	};
+  const openM = () => {
+    setOpen(true);
+  };
 
-	const closeM = () => {
-		setOpen(false);
-	};
+  const closeM = () => {
+    setOpen(false);
+  };
 
-	/* Horas a asignar */
   const [hoursToAssign, setHoursToAssign] = useState(0);
   
   const assignEmployee = async (proyecto, empleado, horas) => {
-		let project_id = proyecto;
-		let employee_id = empleado;
-		let hours_to_assign = horas;
+    let project_id = proyecto;
+    let employee_id = empleado;
+    let hours_to_assign = horas;
 
-		const res = await axios.post(`${url}project/${project_id}/${employee_id}/${hours_to_assign}`)
+    const res = await axios.post(`${url}project/${project_id}/${employee_id}/${hours_to_assign}`)
     setOpen(false)
     setHoursToAssign(0)
     await load()
-	}
+  }
 
-	return (
-		<>
-			<button className='open-modal' onClick={openM}>Asignar</button>
-			<section
-				className="modal"
-				style={open ? { display: 'flex' } : { display: 'none' }}
-			>
-				<div className="modal-container">
+  return (
+    <>
+      <button className='open-modal' onClick={openM}>Asignar</button>
+      <section
+        className="modal"
+        style={open ? { display: 'flex' } : { display: 'none' }}
+      >
+        <div className="modal-container">
           <p>Ingrese la cantidad de horas a asignar</p>
           <h3>{nameSurname}</h3>
           <p>Horas disponibles: <span>{ freeHours }</span></p>
@@ -56,9 +54,9 @@ export default function AssignModal({ idProject, idEmployee, load, nameSurname, 
             </button>
             <button onClick={() => assignEmployee(idProject, idEmployee, hoursToAssign)}> Aceptar </button>
           </div>
-				</div>
-			</section>
-		</>
-	);
+        </div>
+      </section>
+    </>
+  );
 }
 
