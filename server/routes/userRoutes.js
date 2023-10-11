71671aecc00ff4e0439e3a36d950cda44c43d825
SimonSantillan <sgeronimosantillan@outlook.com>
2023-10-11 17:04:02 -0300

    fix db

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.get('/getProjectManagerInfo/:id_user', getProjectManagerInfo);
 router.post('/createUser', createUser);
-router.delete('/deleteUser/:@id_employee', deleteUser);
+router.delete('/deleteUser/:id_employee', deleteUser);
 
 module.exports = router;
