5ae170c76ed30fe08be9395e2c5a326ba5143978
SimonSantillan <sgeronimosantillan@outlook.com>
2023-10-26 14:30:16 -0300

    add pm role

---
+++ client/src/components/pages/project-manager/ShowAssignedProjects.jsx
@@ -0,0 +1,22 @@
+import '../../../assets/css/ShowAssignedProjects.css'
+import Header from '../../common/Header'
+import SidebarPm from '../../common/SidebarPm'
+
+
+
+export default function ShowAssignedProjects() {
+  return (
+		<>
+			<main className="main-home">
+				<SidebarPm></SidebarPm>
+				<section className="section-right">
+					<Header></Header>
+				</section>
+        <section className="projects-assignment-pm">
+          
+          
+        </section>
+			</main>
+		</>
+	);
+}
